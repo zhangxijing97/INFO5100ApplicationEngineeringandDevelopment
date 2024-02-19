6664f335d8f372d13f6cff481ef17eb9da2ae709
Xijing Zhang <zhangxijing97@gmail.com>
2024-02-19 14:52:27 -0800

    Completed HWtoChapter6

--- Assignments/HWtoChapter6/java_projects/myproject/src/main/java/edu/neu/mgen/App.java
+++ Assignments/HWtoChapter6/java_projects/myproject/src/main/java/edu/neu/mgen/App.java
@@ -2,6 +2,9 @@ package edu.neu.mgen;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.time.LocalDateTime;
+// import java.time.LocalDate;
+import java.time.Duration;
 
 /**
  *
@@ -13,9 +16,38 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
 
-        String str = "Oakland";
-        System.out.println("Character at index 2: " + str);
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter any word:");
+        LocalDateTime startTime = LocalDateTime.now(); 
+        String inputWord = scanner.nextLine().trim();
+
+        if (inputWord.isEmpty()) {
+            System.out.println("You did not enter any word");
+        } else {
+            int wordLength = inputWord.length();
+            String wordCategory;
+
+            if (wordLength <= 5) {
+                wordCategory = "short";
+            } else if (wordLength <= 10) {
+                wordCategory = "medium";
+            } else {
+                wordCategory = "long";
+            }
+
+            LocalDateTime endTime = LocalDateTime.now(); 
+
+            Duration duration = Duration.between(startTime, endTime);
+            double reactionTime = duration.getSeconds(); // converting to seconds
+
+            System.out.println("Your word is " + inputWord);
+            System.out.println("The length of the word is " + wordLength);
+            System.out.println("It is a " + wordCategory + " word");
+            System.out.println("Your reaction time is " + reactionTime + " seconds");
+        }
+
+        scanner.close();
 
     }
 }
